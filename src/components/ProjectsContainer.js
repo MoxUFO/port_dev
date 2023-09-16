f749c0d83635f24dfbf38fee688fa81b2a3ea979
MOX UFO <mox.ufo@gmail.com>
2023-09-16 16:21:06 -0400

    added background to project sections, extended app and portfolio background height

---
+++ src/components/ProjectsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../assets/css/ProjectContainer.css'
+import Paper from '@mui/material/Paper';
+
+
+
+export default function ProjectsContainer() {
+  return (
+    <Paper className='project-bg' />
+  )
+}
