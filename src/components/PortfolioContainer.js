0d0a00ffa6b2c27ab954ee1fbbd4d6f1f45df4ac
MOX UFO <mox.ufo@gmail.com>
2023-09-16 15:03:11 -0400

    added components and files to start the first edition of the app background

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import '../assets/css/PortfolioContainer.css'
+
+export default function PortfolioContainer() {
+  return (
+    <Paper className='port-container' elevation={3} >
+    </Paper>
+    
+  )
+}
