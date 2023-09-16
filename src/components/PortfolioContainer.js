7ff9f76ffbbd3de1cfc45be51579174d64171c5b
MOX UFO <mox.ufo@gmail.com>
2023-09-16 15:25:29 -0400

    add background image for intro,adjust app overall background

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import '../assets/css/PortfolioContainer.css'
+import Intro from './Intro';
 
 export default function PortfolioContainer() {
   return (
     <Paper className='port-container' elevation={3} >
+        <Intro />
     </Paper>
     
   )
