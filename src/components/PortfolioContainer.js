2e4e4ad92980fe2913fd1f7c62c74a9ce3edda8d
MOX UFO <mox.ufo@gmail.com>
2023-09-16 15:39:28 -0400

    header for project portion of portfolio

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -2,11 +2,14 @@ import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import '../assets/css/PortfolioContainer.css'
 import Intro from './Intro';
+import ProjectHeader from './ProjectHeader';
+
 
 export default function PortfolioContainer() {
   return (
     <Paper className='port-container' elevation={3} >
         <Intro />
+        <ProjectHeader />
     </Paper>
     
   )
